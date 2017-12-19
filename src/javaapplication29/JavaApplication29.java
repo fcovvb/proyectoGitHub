9eabd4d0fe3f9cb2c6bbe671ed9d2ad06d27ffeb
fcovvb <fcovvb@gmail.com>
2017-12-19 20:12:59 -0300

    Inicio Frame desde Main

--- src/javaapplication29/JavaApplication29.java
+++ src/javaapplication29/JavaApplication29.java
@@ -1,5 +1,9 @@
 package javaapplication29;
 import java.util.Random;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 //hola soy german
 /**
  *
@@ -11,7 +15,17 @@ public class JavaApplication29 {
 
 
     public static void main(String[] args) {
-      
+      NewJFrame frame = new NewJFrame();
+      try {
+           UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+           SwingUtilities.updateComponentTreeUI(frame);
+           frame.setTitle("StarFrase");
+        } catch (UnsupportedLookAndFeelException ex) {}
+         catch (ClassNotFoundException ex) {}
+         catch (InstantiationException ex) {}
+         catch (IllegalAccessException ex) {}
+       
+      frame.setVisible(true);
     }
    
 }
